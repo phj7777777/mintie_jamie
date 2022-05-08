767dfecf52dce34168dbbf6ac6d839bbe87a3b7d
PHJ <haojie02074@gmail.com>
2022-05-08 21:57:37 +0800

    Add auth subsribe

--- src/app/pages/others/pages-routing.module.ts
+++ src/app/pages/others/pages-routing.module.ts
@@ -1,53 +1,59 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-import { AboutOneComponent } from './about-one/about-one.component';
-import { AboutTwoPageComponent } from './about-two/about-two.component';
-import { LoginPageComponent } from './login/login.component';
-import { FaqsPageComponent } from './faqs/faqs.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { ContactOnePageComponent } from './contact-one/contact-one.component';
-import { ContactTwoPageComponent } from './contact-two/contact-two.component';
+import {AboutOneComponent} from './about-one/about-one.component';
+import {AboutTwoPageComponent} from './about-two/about-two.component';
+import {LoginPageComponent} from './login/login.component';
+import {FaqsPageComponent} from './faqs/faqs.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {ContactOnePageComponent} from './contact-one/contact-one.component';
+import {ContactTwoPageComponent} from './contact-two/contact-two.component';
+import {ProfileComponent} from '../profile/profile.component';
 
 const routes: Routes = [
-    {
-        path: '',
-        redirectTo: 'about',
-        pathMatch: 'full'
-    },
-    {
-        path: 'about',
-        component: AboutOneComponent
-    },
-    {
-        path: 'about-2',
-        component: AboutTwoPageComponent
-    },
-    {
-        path: '404',
-        component: PageNotFoundComponent
-    },
-    {
-        path: 'login',
-        component: LoginPageComponent
-    },
-    {
-        path: 'faq',
-        component: FaqsPageComponent
-    },
-    {
-        path: 'contact',
-        component: ContactOnePageComponent
-    },
-    {
-        path: 'contact-2',
-        component: ContactTwoPageComponent
-    }
+  {
+    path: '',
+    redirectTo: 'about',
+    pathMatch: 'full'
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent
+  },
+  {
+    path: 'about',
+    component: AboutOneComponent
+  },
+  {
+    path: 'about-2',
+    component: AboutTwoPageComponent
+  },
+  {
+    path: '404',
+    component: PageNotFoundComponent
+  },
+  {
+    path: 'login',
+    component: LoginPageComponent
+  },
+  {
+    path: 'faq',
+    component: FaqsPageComponent
+  },
+  {
+    path: 'contact',
+    component: ContactOnePageComponent
+  },
+  {
+    path: 'contact-2',
+    component: ContactTwoPageComponent
+  }
 ];
 
 @NgModule({
-    imports: [RouterModule.forChild(routes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
 })
 
-export class PagesRoutingModule { };
+export class PagesRoutingModule {
+};
